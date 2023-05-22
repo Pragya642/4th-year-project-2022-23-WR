d108a1d8d460760d6edc132d7ad962610490bf8d
Pragya Das <61628255+Pragya642@users.noreply.github.com>
2023-05-22 22:20:19 +0530

    Add files via upload

---
+++ FeatureDatasetExtractor.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+from keras import backend as K
+
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+
+from tensorflow.keras.preprocessing.image import load_img
+#from tensorflow.keras.preprocessing.image import image_dataset_from_directory
+
+#from tensorflow.keras import backend as K
+#from keras.preprocessing.image import load_img
+#from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+
+#import tensorflow.keras.backend as K
+from keras.utils import load_img, img_to_array
+
+import numpy as np
+import csv
+import os
+
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
+
+print(device_lib.list_local_devices())
+
+#K.tensorflow_backend._get_available_gpus()
+
+# constant for target size for resizing image
+TARGET_SIZE = (224, 224)
+
+# vgg16 data extraction Model
+DataExtractionModel_VGG16 = VGG16()
+DataExtractionModel_VGG16.layers.pop()
+DataExtractionModel_VGG16 = Model(inputs = DataExtractionModel_VGG16.inputs, outputs = DataExtractionModel_VGG16.layers[-2].output)
+#DataExtractionModel_VGG16 = VGG16(weights='imagenet', include_top=False)
+
+# function to save extracted features as csv file
+# produces a csv file where each row represents features of a single word image
+# each row contains 4096 features in total
+
+DataExtractionModel_VGG16.summary()
+
+def saveFeatureDatasetCSV(predictionModel, inputFolder, outputFilePath, imageTargetSize):
+
+    # load all images in one array
+    print("loading data...")
+    imageNames = os.listdir(inputFolder)
+    count = len(imageNames)
+    images = []  # target image array
+
+    for imageName in imageNames:
+        imagePath = inputFolder + '/' + imageName
+        image = load_img(imagePath, target_size=imageTargetSize)
+        image = img_to_array(image)
+        images.append(image)
+
+    print("Total " + str(count) + " no. of images loaded!")
+    # prepare images array to pass to the feature extructor model
+    print("Preparing data...")
+    images = np.array(images)
+    images = preprocess_input(images)
+
+    # extract features
+    print("Extacting features...")
+    features = predictionModel.predict(images)
+
+    # dump to csv file
+    print("Saving data...")
+    with open(outputFilePath, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for feature in features:
+            writer.writerow(feature)
+        file.close()
+    print("Done!")
---
+++ VGG16_train.py
@@ -0,0 +1,115 @@
+import os
+import keras
+from PIL import Image
+
+
+from keras.preprocessing import image
+
+from keras.utils import load_img
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+
+from keras.utils import load_img, img_to_array
+
+# linear algebra mathematics
+import numpy as np
+
+# data representation, data store, data handling
+import pandas as pd
+
+# graphs drawing
+import matplotlib.pyplot as plt
+
+# beautifies graphs based on seaborn
+import seaborn as sns
+
+# initialises seaborn
+sns.set()
+
+DataExtractionModel_VGG16 = VGG16()
+DataExtractionModel_VGG16.layers.pop()
+
+# top is not included as we are
+model = VGG16(weights='imagenet', include_top=False)
+# model = Model(inputs = DataExtractionModel_VGG16.inputs, outputs = DataExtractionModel_VGG16.layers[-1].output)
+
+# dealing with only feature extraction
+model.summary()
+
+# prepares images for loading into the model into the program
+
+
+def image_loader(img_path):
+
+    # uses the keras preprocessing library to prepare the images for loading
+    # here the image path is loaded and the target size is made 224 224 acc to vgg 16 specs
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # print(img.shape)
+    # gives error as the object is 'Image', not array
+    # shape: numpy method, input array, no of rows and column
+
+    # shows the image
+    plt.imshow(img)
+
+    # converts the image to an array
+    img_data = image.img_to_array(img)
+
+    # prints the shape
+    print(img_data.shape)
+
+    # converts the image array into a vector from a single dimensional array
+    img_data = np.expand_dims(img_data, axis=0)  # google axis = 0
+
+    # prints the shape
+    print(img_data.shape)
+
+    # google
+    img_data = preprocess_input(img_data)
+
+    print(img_data.shape)
+    return img_data
+
+
+img_path = '../Datasets/writerPair1/0-10/train/0/0000_01_0.tif'
+img_data = image_loader(img_path)
+
+# using the pretrained vgg16 model to output the feature matrix of one single image for testing
+model_feature = model.predict(img_data)
+
+feature_matrix = model_feature.flatten()
+
+print(model_feature.shape)
+print(feature_matrix.shape)
+
+np.save("feature.npy", model_feature)
+
+df = pd.DataFrame(feature_matrix)
+df.to_csv('feature.csv', index=False)
+
+
+root_dir_train = '../Datasets/writerPair1/0-10/train/0'
+
+'''for subdir, dirs, files in os.walk(root_dir_train):
+    for file in files:
+      path = os.path.join(subdir, file)
+      print("Processing: {}".format(path))
+      img_data = image_loader(path)
+      
+      feature = model.predict(img_data)
+      feature_np = np.array(feature)
+      features.append(feature_np.flatten())'''
+
+
+datagen = ImageDataGenerator(rescale=1./255)
+generator = datagen.flow_from_directory(
+    root_dir_train,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='sparse',
+    shuffle=True)
+
+for x, y in generator:
+    print(y)
